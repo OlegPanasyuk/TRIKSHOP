aaf56e62ec7251e50755ef99c90c7aeeb15acf47
OlegPanasyuk <oleg.panasyuk@inbox.ru>
2018-09-27 08:24:52 +0300

    Add some fatures like bar menu close by clicking link

--- TEMP/HEADER/JS/header.js
+++ TEMP/HEADER/JS/header.js
@@ -1,9 +1,27 @@
 init__header();
 
 function init__header() {
-  let menu_bar = document.querySelector('.header__menu_bar');
+  let  menu_bar = document.querySelector('.header__menu_bar');
   let  menu = document.querySelector('.header__menu');
+  let  links = document.querySelectorAll('.header__menu a');
   menu_bar.addEventListener('click', function(event) {
     menu.classList.toggle('header__menu_closed');
+    // if ( menu.classList.contains('header__menu_closed')) {
+    //   menu_bar.innerHTML = ' <i class="fas fa-bars"></i>';
+    // } else {
+    //   menu_bar.innerHTML = '<i class="fas fa-times"></i>';
+    // }
+    
   })
+
+  links.forEach(function(el) {
+    el.addEventListener('click', function(event) {
+      menu.classList.toggle('header__menu_closed');
+      // if ( menu.classList.contains('header__menu_closed')) {
+      //   menu_bar.innerHTML = ' <i class="fas fa-bars"></i>';
+      // } else {
+      //   menu_bar.innerHTML = '<i class="fas fa-times"></i>';
+      // }
+    });
+  });
 }
